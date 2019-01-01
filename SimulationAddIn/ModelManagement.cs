83470905cc5e522f549bcf1ea2ee437d2d62e07b
Todd Johnson <mr.todd.johnson@gmail.com>
2018-12-31 17:48:38 -0700

    Started adding <html> for help

--- SimulationAddIn/ModelManagement.cs
+++ SimulationAddIn/ModelManagement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace SimulationAddIn
 {
@@ -14,8 +15,21 @@ namespace SimulationAddIn
         {
             string modelPath = ConfigRep.ReadModelPathFromSheet();
             string modelName = ConfigRep.ReadAutoModFileFromSheet();
+            string fullPath = modelPath + "\\" + modelName + ".exe";
 
             MessageBox.Show(modelPath + "\\" + modelName + ".exe" );
+
+            Process p = new Process();
+
+            p.StartInfo.FileName = fullPath;
+            p.EnableRaisingEvents = true;
+            p.Exited += new EventHandler(RunHasExited);
+            p.Start();
+        }
+
+        private void RunHasExited(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Simulation has exited");
         }
     }
 }
